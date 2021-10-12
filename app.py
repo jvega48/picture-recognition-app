53893b6c0859a848c15ae370e56bcea22fb63f48
jvega48 <jvega4849@gmail.com>
2021-10-11 22:23:15 -0700

    Temp enpoints

--- app.py
+++ app.py
@@ -1,117 +1,193 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request
 import logging
 import mysql.connector
 import json
 from flask_cors import CORS
 from flask_restful import reqparse, Api, Resource
 from mysql.connector.errors import Error
+import logging
+logging.basicConfig(level=logging.INFO)
 
 server = Flask(__name__)
-
-# set up home route the app
-
-@server.route("/")
-def home():
-    logging.info("logging into the home page")
-    return "Home"
-
-
-
-
-
-class db_operations(self):
-    def db_cursor(database_name, table_name, table_info):
-        cursor = None
-        if database_name is None or database_name is None:
-            return NotImplementedError
-        
-        if database_name is None:
-            try:
-                db_connector = mysql.connector.connect(
-                    host='mysqldb',
-                    user='root',
-                    password='password',
-                    database=database_name)
-                cursor = db_connector.cursor()
-                return cursor
-            except mysql.connector.Error as err:
-                print("Something went wrong: {}".format(err))
-                return
-        elif database_name and table_name is not None:
-            str_query_drop_table = "DROP TABLE IF EXISTS{}".format(table_name)
-            str_query_create_table = "CREATE TABLE {} (name VARCHAR(255), url VARCHAR(255))".format(table_name)
-            try:
-                db_connector = mysql.connector.connect(
-                    host='mysqldb',
-                    user='root',
-                    password='password',
-                    database=database_name)
-                cursor = db_connector.cursor()
-                return cursor
-            except mysql.connector.Error as err:
-                print("Something went wrong: {}".format(err))
-                return
-
-            cursor.close()
-
-            cursor.execute(str_query_drop_table)
-            cursor.execute(str_query_create_table)
-            cursor.close()
-
-        # TODO: need to figure out the  error handling for issues with the connections 
-        return False
-
-    def insert_photo(name, url):
-        database_name='images_core'
-        cursor = db_cursor(database_name)
-        str_query = "INSERT into photos(name, url) VALUES ({},{});".format(name, url)
-        
-        if cursor is None:
-            return str(Error("Db cursor is none cannot continue"))
+api = Api(server)
+class DBOperations(Resource):
+    def __init__(self):
+        return
+    
+    def check_existing_table(self, database_name, table_name):
         try:
-            cursor.excute(str_query)
-            cursor.commit()
+            db_connector = self.db_connector()
+            crs = db_connector.cursor()
+            crs.execute("SHOW TABLES")
+
+            for x in crs:
+                if x[0] == table_name:
+                    return True
+        except mysql.connector.Error as err:
+            logging.info('error in the connector... %s', err)
+            print("Something went wrong: {}".format(err))
+            return None 
+
+    # def create_table(self, database_name, table_name, fields):
+    #     is_database = self.check_existing_db(database_name)
+    #     connected_to_database = self.connect_to_database(database_name)
+    #     if is_database:
+    #         check_table = self.check_existing_table(table_name)
+    #         if check_table is False:
+    #             if connect_to_database:
+    #                 crs = self.db_cursor(database_name)
+    #                 try:
+    #                     crs.execute("CREATE TABLE {} ({} {}, address VARCHAR(255))".format(table_name))
+    #                 except mysql.connector.Error as err:
+    #                     logging.info('error in the connector... %s', err)
+    #                     print("Something went wrong: {}".format(err))
+    #                 return None 
+
+    def check_existing_db(self, database_name):
+        try:
+            db_connector = self.db_connector()
+            crs = db_connector.cursor()
+            crs.execute("SHOW DATABASES")
+            
+            for x in crs:
+                if x[0]==database_name:
+                    return True                
             return True
         except mysql.connector.Error as err:
-            print("Something went wrong when inserting record: {}".format(err))
+            logging.info('error in the connector... %s', err)
+            print("Something went wrong: {}".format(err))
+            return None 
+    
+    # connected true else False
+    def connect_to_database(self, database_name):
+        try:
+            db_connector = mysql.connector.connect(
+                host='mysqldb',
+                user='root',
+                password='password',
+                database=database_name)
+            return True
+        except mysql.connector.Error as err:
+            logging.info('error in the connector... %s', err)
+            print("Something went wrong: {}".format(err))
             return False
 
-    def get_all_images():
-        mydb = mysql.connector.connect(
-            host='mysqldb',
-            user='root',
-            password='password',
-            database='images_core')
-        cursor = mydb.cursor()
-
-
-        cursor.execute("SELECT * FROM photos")
-
-        #describ the headers
-        row_headers = [x[0] for x in cursor.description]
-        results = cursor.fetchall()
-
-        json_data = []
-
-        for result in results:
-            json_data.append(dict(zip(row_headers, result)))
-
-        cursor.close()
-
-        return json.dumps(json_data)
+    def db_connector(self):
+        try:
+            db_connector = mysql.connector.connect(
+                host='mysqldb',
+                user='root',
+                password='password')
+            return db_connector
+        except mysql.connector.Error as err:
+            logging.info('error in the connector... %s', err)
+            print("Something went wrong: {}".format(err))
+            return None
     
+    def db_cursor(self, database_name):
+
+        logging.info('inisde the Init db...')
+        # step one check if the database exist already 
+        # if databse exist already don't create one 
+        # if database exist and table doestn exisit
+        # create a new table if datbase exisit
+        # if dababse doesnt exist then table wont exist
+        # create both 
+        if database_name:
+            found = self.check_existing_db(database_name)
+            if found:
+                try:
+                    db_connector = mysql.connector.connect(
+                        host='mysqldb',
+                        user='root',
+                        password='password',
+                        database=database_name)
+                    return db_connector
+                except mysql.connector.Error as err:
+                    logging.info('error in the connector... %s', err)
+                    print("Something went wrong: {}".format(err))
+                    return None
+            else:
+                try:
+                    db_connector = self.db_connector()
+                    cur = db_connector.cursor()
+                    cur.execute("CREATE DATABASE {}".format(database_name))
+                    return db_connector,
+                except mysql.connector.Error as err:
+                    logging.info('error in the connector... %s', err)
+                    print("Something went wrong: {}".format(err))
+                    return None
+                
+        else:
+            return self.db_connector()
+
+        # TODO: need to figure out the  error handling for issues with the connections 
+class InsertPhotos(Resource):
+    def insert_photo(self, name, url):
+        db_opr = DBOperations()
+        database_name='images_core'
+        con = db_opr.db_cursor(database_name)
+        cursor = con.cursor()
+        if con is None:
+            return str(Error("Db cursor is none cannot continue."))
+
+        str_query = "INSERT into photos(name, url) VALUES (%s, %s);"
+        
+        if con is None:
+            return str(Error("Db cursor is none cannot continue"))
+        try:
+            cursor.execute(str_query, (name,url))
+            con.commit()
+            logging.info("record inserted. %s", cursor.rowcount, )
+            return str("Db insert documents successful : {}".format(True))
+        except mysql.connector.Error as err:
+            print("Something went wrong when inserting record: {}".format(err))
+            return str("Db insert documents successful : {}".format(False))
+    
+    def post(self, name, url):
+        message = self.insert_photo(name, url)
+        return jsonify({'message': message})
+
+    # def get(self):
+    #     data = GetPhotos.get_all_images()
+    #     return jsonify({'data': data})
+class GetPhotos(Resource):
+    def get_all_images(self):
+        db_opr = DBOperations()
+        database_name ='images_core'
+        con = db_opr.db_cursor(database_name)
+
+        if con is None:
+            return str(Error("Db cursor is none cannot continue. {}".format(con)))
+        
+        query = con.cursor(buffered=True).execute("SELECT * FROM photos")
+        results = None
+
+        if query:        
+            row_headers = [x[0] for x in con.cursor().description]
+            results = con.cursor().fetchall()
+        
+        json_data = []
+        if results:
+            for result in results:
+                json_data.append(result)
 
 
-@server.route('/init_db')
-def db_init():
-    object_a = db_operations()
-    print(object_a)
-    e = object_a.db_cursor('images_core', 'photos')
-    if e:
-        return 'non init database'
-    return 'init database'
+            return json.dumps(json_data)
+        else:
+            return json.dumps(json_data)
+    
+    # def post(self, name, url):
+    #     message = InsertPhotos.insert_photo(name, url)
+    #     return jsonify({'message': message})
 
+    def get(self):
+        data = self.get_all_images()
+        return jsonify({'data': data})
+
+api.add_resource(InsertPhotos, '/insert_photo/<string:name>/<string:url>/')
+api.add_resource(GetPhotos, '/get_all_photos')
 
 
 if __name__ == '__main__':
